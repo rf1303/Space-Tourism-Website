dc9d2724d85d288ff4a93fe5c74b788b983a93d8
Ramiro Fernandez <pulgaramiro@gmail.com>
2025-09-03 14:10:50 -0300

    modificando bg 05

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,12 +14,17 @@ module.exports = {
 
     theme: {
         extend: {
+            backgroundImage: {
+                "tech-mobile": "url('./assets/images/technology/background-technology-mobile.jpg')",
+                "tech-tablet": "url('./assets/images/technology/background-technology-tablet.jpg')",
+                "tech-desktop": "url('./assets/images/technology/background-technology-desktop.jpg')",
+            },
         },
     },
     plugins: [
     ],
     corePlugins: {
-    backdropFilter: true,
-  },
+        backdropFilter: true,
+    },
 }
 
