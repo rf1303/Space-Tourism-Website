b906a2b3caf2341307b3f84e0c61b5ffe43747f2
Ramiro Fernandez <pulgaramiro@gmail.com>
2025-09-02 23:51:10 -0300

    detalles htmls

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,20 +8,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const currentPage = window.location.pathname.split("/").pop();
     const menuMain = document.querySelectorAll('nav a');
-    const textMoon = document.querySelectorAll('.text__moon');
-
-    textMoon.forEach( btn => {
-        btn.addEventListener('click', () => {
-            textMoon.forEach(e => {
-                e.setAttribute('aria-selected', 'false');
-                e.parentElement.classList.remove("active");
-            });
-
-            btn.setAttribute('aria-selected', 'true');
-            btn.parentElement.classList.add("active");
-        });
-    });
-
 
     menuMain.forEach(e => {
         const href = e.getAttribute("href").split("/").pop();
@@ -32,6 +18,5 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log('No corresponde', href);
         }
     });
-
 
 });
