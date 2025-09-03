7d36f6d78061ac739a54eaa93c2224d1ef973a72
Ramiro Fernandez <pulgaramiro@gmail.com>
2025-09-03 11:37:23 -0300

    crew.js technology.js + detalles

---
+++ assets/js/crew.js
@@ -0,0 +1,44 @@
+import { dataSpace } from "./data-change.js";
+
+const btnDots = document.querySelectorAll('.btn__dots');
+
+btnDots.forEach(btn => {
+    btn.addEventListener('click', () => {
+    console.log('btnDots')
+        btnDots.forEach(e => {
+            e.setAttribute('aria-selected', 'false');
+        });
+
+        btn.setAttribute('aria-selected', 'true');
+
+        const dataName = btn.dataset.crew;
+        console.log('data-crew: ', dataName, btn);
+
+        (async () => {
+            await dataCrew(dataName);
+        })();
+    });
+});
+
+export const dataCrew = async (dataName) => {
+    
+    const crews = await dataSpace("crew");
+    console.log('crews: ',crews[dataName]);
+    if (!dataCrew) return;
+
+    const dataTitle = document.getElementById('data__name');
+    const dataRole = document.getElementById('data__role');
+    const dataImg = document.getElementById('data__img');
+    const imgPng = document.getElementById('img__png');
+    const imgWebp = document.getElementById('img__webp');
+    const dataBio = document.getElementById('data__bio');
+
+    dataTitle.textContent = crews[dataName].name;
+    dataRole.textContent = crews[dataName].role;
+    dataBio.textContent = crews[dataName].bio;
+    dataImg.src = crews[dataName].images.webp;
+    imgWebp.src = crews[dataName].images.webp;
+    imgPng.src = crews[dataName].images.png;
+
+};
+
---
+++ assets/js/technology.js
@@ -0,0 +1,42 @@
+import { dataSpace } from "./data-change.js";
+
+const btnNbrs = document.querySelectorAll('.btn__nbrs');
+
+btnNbrs.forEach(btn => {
+    btn.addEventListener('click', () => {
+    console.log('btnDots')
+        btnNbrs.forEach(e => {
+            e.setAttribute('aria-selected', 'false');
+        });
+
+        btn.setAttribute('aria-selected', 'true');
+
+        const dataName = btn.dataset.techn;
+        console.log('data-techn: ', dataName, btn);
+
+        (async () => {
+            await dataTechn(dataName);
+        })();
+    });
+});
+
+export const dataTechn = async (dataName) => {
+    
+    const technologys = await dataSpace("technology");
+    console.log('technologys: ',technologys[dataName]);
+    if (!dataTechn) return;
+
+    const dataTitle = document.getElementById('data__name');
+    const dataImg = document.getElementById('data__img');
+    const imgPortrait = document.getElementById('img__portrait');
+    const imgLandscp = document.getElementById('img__landscp');
+    const dataDescrp = document.getElementById('data__description');
+
+    dataTitle.textContent = technologys[dataName].name;
+    dataDescrp.textContent = technologys[dataName].description;
+    dataImg.src = technologys[dataName].images.portrait;
+    imgPortrait.srcset = technologys[dataName].images.portrait;
+    imgLandscp.srcset = technologys[dataName].images.landscape;
+
+};
+
